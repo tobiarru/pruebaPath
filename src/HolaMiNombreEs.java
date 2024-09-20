9dae0d7286deb90bb9a79a848165aaf6344b1c5e
tobiarru <tobiasarrussi@gmail.com>
2024-09-20 16:48:20 -0300

    probandoPat

---
+++ src/HolaMiNombreEs.java
@@ -0,0 +1,8 @@
+public class HolaMiNombreEs {
+    public static void main(String[] args) {
+
+        String nombre = "el Tobens";
+        System.out.println("nombre = " + nombre);
+        System.out.println("Mi Nombre es " + nombre);
+    }
+}
